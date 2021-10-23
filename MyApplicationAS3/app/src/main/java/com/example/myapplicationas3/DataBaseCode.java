f108c7910761ce357dfd31f7d6400910b1f406e7
SarbjotS <62689363+SarbjotS@users.noreply.github.com>
2021-10-23 15:09:34 +1300

    Buttons done
    DB code next

---
+++ MyApplicationAS3/app/src/main/java/com/example/myapplicationas3/DataBaseCode.java
@@ -0,0 +1,4 @@
+package com.example.myapplicationas3;
+
+public class DataBaseCode {
+}
--- MyApplicationAS3/app/src/main/java/com/example/myapplicationas3/LoginPage.java
+++ MyApplicationAS3/app/src/main/java/com/example/myapplicationas3/LoginPage.java
@@ -3,12 +3,37 @@ package com.example.myapplicationas3;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class LoginPage extends AppCompatActivity {
 
+    EditText userName, Password, ReEnterPassword;
+    Button Register, SignIn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_page);
+
+        userName = (EditText) findViewById(R.id.userName);
+        Password = (EditText) findViewById(R.id.Password);
+        ReEnterPassword = (EditText) findViewById(R.id.ReEnterPassword);
+        Register = (Button) findViewById(R.id.Register);
+        SignIn = (Button) findViewById(R.id.SignIn);
+
+        Register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        SignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
