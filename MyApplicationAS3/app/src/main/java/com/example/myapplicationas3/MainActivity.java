052429ce685baefe20dbee3cd2a84bb622bdd730
Caanite <61674541+Caanite@users.noreply.github.com>
2021-10-23 16:37:03 +1300

    Got changing views working
     Overthought it a lot

--- MyApplicationAS3/app/src/main/java/com/example/myapplicationas3/MainActivity.java
+++ MyApplicationAS3/app/src/main/java/com/example/myapplicationas3/MainActivity.java
@@ -3,12 +3,24 @@ package com.example.myapplicationas3;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button EntryAdder;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        EntryAdder = findViewById(R.id.button);
+        EntryAdder.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setContentView(R.layout.activity_add);
+            }
+        });
     }
 }
