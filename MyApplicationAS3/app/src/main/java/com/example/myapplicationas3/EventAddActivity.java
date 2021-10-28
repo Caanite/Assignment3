ec9a45d8894cf861d1806caa9072e91b3c1c8ced
Caanite <61674541+Caanite@users.noreply.github.com>
2021-10-28 21:11:01 +1300

    Theoretically should change text based on option selected in repeat dialog

---
+++ MyApplicationAS3/app/src/main/java/com/example/myapplicationas3/EventAddActivity.java
@@ -0,0 +1,51 @@
+package com.example.myapplicationas3;
+
+import android.content.DialogInterface;
+import android.content.res.Resources;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class EventAddActivity extends AppCompatActivity {
+
+    private EditText eventName;
+    private EditText eventDate;
+    private Button repeatButton;
+    private TextView repeatOptionText;
+    private Button addEvent;
+
+    private String[] arrayTimeTexts;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add);
+
+        Resources res = getResources();
+
+        repeatButton = findViewById(R.id.repeatButton);
+        repeatButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(repeatButton.getContext());
+
+                arrayTimeTexts = res.getStringArray(R.array.Time_array);
+
+                builder.setMessage(R.string.repeatText).setItems(R.array.Time_array, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        repeatOptionText.setText(arrayTimeTexts[which]);
+                    }
+                });
+
+                builder.create();
+            }
+        });
+    }
+}
