da7a72239606427a3f15474725bc77df5839963a
SarbjotS <62689363+SarbjotS@users.noreply.github.com>
2021-10-29 20:10:43 +1300

    Added both permissions
    might not need them

--- MyApplicationAS3/app/src/main/java/com/example/myapplicationas3/EventAddActivity.java
+++ MyApplicationAS3/app/src/main/java/com/example/myapplicationas3/EventAddActivity.java
@@ -1,9 +1,16 @@
 package com.example.myapplicationas3;
 
+import android.Manifest;
+import android.annotation.TargetApi;
 import android.app.DatePickerDialog;
 import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.content.res.Resources;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.util.EventLog;
 import android.view.View;
 import android.widget.Button;
 import android.widget.DatePicker;
@@ -11,8 +18,11 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 import java.time.Year;
 import java.util.Calendar;
@@ -21,13 +31,14 @@ import java.text.SimpleDateFormat;
 
 public class EventAddActivity extends AppCompatActivity {
 
+    private static final int _Request = 1;
     private EditText eventName;
-    private EditText eventDate;
     private Button repeatButton;
     private TextView repeatOptionText;
     private Button addEvent;
     private Calendar Cal = Calendar.getInstance();
     private int yr = Cal.get(Calendar.YEAR), mon = Cal.get(Calendar.MONTH), day = Cal.get(Calendar.DAY_OF_MONTH);
+    private boolean granted = false;
 
     private String[] arrayTimeTexts;
 
@@ -52,9 +63,9 @@ public class EventAddActivity extends AppCompatActivity {
                 Cal.set(Calendar.YEAR, year);
                 Cal.set(Calendar.MONTH, month);
                 Cal.set(Calendar.DAY_OF_MONTH, dayOfMonth);
-                if (year>=yr && dayOfMonth>=day && month>= mon) {
+                if (year >= yr && dayOfMonth >= day && month >= mon) {
                     InputLabel();
-                }else{
+                } else {
                     Toast WrongDateToast = Toast.makeText(getApplicationContext(), "Please make events for the future, not the past", Toast.LENGTH_SHORT);
                     WrongDateToast.show();
                 }
@@ -62,13 +73,13 @@ public class EventAddActivity extends AppCompatActivity {
             }
 
             private void InputLabel() {
-                String myFormat = "MM/dd/yy"; //In which you need put here
+                String myFormat = "dd/MM/yy"; //In which you need put here
                 SimpleDateFormat sdf = new SimpleDateFormat(myFormat, Locale.ENGLISH);
-
                 ed.setText(sdf.format(Cal.getTime()));
 
             }
         };
+
 
         ed.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -89,6 +100,19 @@ public class EventAddActivity extends AppCompatActivity {
         repeatOptionText.setText(repeatText);
     }
 
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_CALENDAR) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_CALENDAR}, _Request);
+        }
+
+    }
 
 }
+
+        // Other 'case' lines to check for other
+        // permissions this app might request.
+
+
 
