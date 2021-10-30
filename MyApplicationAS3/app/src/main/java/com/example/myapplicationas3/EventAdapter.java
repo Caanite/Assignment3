4bd6e6a394aea33a3b7da4c3f46caa9d1a0525b2
Caanite <61674541+Caanite@users.noreply.github.com>
2021-10-30 21:48:37 +1300

    Adapter written, untested as of now

---
+++ MyApplicationAS3/app/src/main/java/com/example/myapplicationas3/EventAdapter.java
@@ -0,0 +1,65 @@
+package com.example.myapplicationas3;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.text.Layout;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+public class EventAdapter extends BaseAdapter {
+
+    Context context;
+    Cursor results;
+    LayoutInflater inflater;
+
+    class ViewHolder
+    {
+        TextView eventName;
+        TextView eventDate;
+    }
+
+    public EventAdapter(Context applicationContext, Cursor results)
+    {
+        this.context = applicationContext;
+        this.results = results;
+        inflater = (LayoutInflater.from(applicationContext));
+    }
+
+    @Override
+    public int getCount()
+    {
+        return results.getCount();
+    }
+
+    @Override
+    public Object getItem(int i)
+    {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int i)
+    {
+        return i;
+    }
+
+    @Override
+    public View getView(int i, View view, ViewGroup viewGroup)
+    {
+        ViewHolder viewHolder;
+        if(view == null)
+        {
+            view = inflater.inflate(R.layout.events, viewGroup, false);
+        }
+
+        results.moveToPosition(i);
+
+        ((TextView)view.findViewById(R.id.eventNameText)).setText(results.getString(0));
+        ((TextView)view.findViewById(R.id.eventDateText)).setText(results.getString(1));
+
+        return view;
+    }
+}
--- MyApplicationAS3/app/src/main/java/com/example/myapplicationas3/MainActivity.java
+++ MyApplicationAS3/app/src/main/java/com/example/myapplicationas3/MainActivity.java
@@ -3,18 +3,29 @@ package com.example.myapplicationas3;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
 
     private Button EntryAdder;
-
+    SQLiteDatabase db = openOrCreateDatabase("database.db", MODE_PRIVATE, null);
+    EventAdapter eventAdapter;
+    ListView events;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Cursor resultSet = db.rawQuery("Select * from events", null);
+        events = findViewById(R.id.events);
+        eventAdapter = new EventAdapter(this, resultSet);
+        events.setAdapter(eventAdapter);
+
 
         EntryAdder = findViewById(R.id.button);
         EntryAdder.setOnClickListener(new View.OnClickListener() {
@@ -25,5 +36,7 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+
+
     }
 }
